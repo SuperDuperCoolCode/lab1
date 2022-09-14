1e638847e67aba7e0c763963e3444cfabfa90e34
Lionel Han <47760119+HanLionel@users.noreply.github.com>
2022-09-13 20:31:32 -0700

    Create CupcakeShop.java

---
+++ CupcakeShop.java
@@ -0,0 +1,16 @@
+package lab1;
+
+/**
+ * This is our top-level class that represents a CupcakeShop!
+ */
+public class CupcakeShop {
+
+    public CupcakeShop() {
+        // Instantiates an instance of our baker as a local variable
+        Baker baker = new Baker();
+
+        //TODO Tell the baker to bake a cupcake
+
+        //TODO Have the decorator and froster do their jobs here
+    }
+}
