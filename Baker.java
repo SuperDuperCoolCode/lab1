43c82e586655d04d58b661dec2898a6e51b9f52d
Lionel Han <47760119+HanLionel@users.noreply.github.com>
2022-09-13 20:31:14 -0700

    Create Baker.java

---
+++ Baker.java
@@ -0,0 +1,17 @@
+package lab1;
+
+//TODO Import CS15Cupcake
+
+/**
+ * This class represents our Baker, who will bake cupcakes on command!
+ */
+public class Baker {
+
+  public Baker() {
+  }
+
+  //TODO Edit the return type to match the JavaDocs
+  public void bakeCupcake() {
+    //TODO Instantiate and return a new CS15Cupcake
+  }
+}
