cc9f849facb4b10c63e73b8d6440649c11201cc4
Lionel Han <47760119+HanLionel@users.noreply.github.com>
2022-10-04 18:33:09 -0700

    Update CupcakeShop.java

--- CupcakeShop.java
+++ CupcakeShop.java
@@ -1,16 +1,42 @@
 package lab1;
 
+import cs15.labs.cupcakeSupport.CS15Decorator;
+import cs15.labs.cupcakeSupport.CupcakeShopSupport;
+
 /**
  * This is our top-level class that represents a CupcakeShop!
  */
-public class CupcakeShop {
+public class CupcakeShop extends CupcakeShopSupport {
 
+    private CS15Decorator myDecorator;
+    
     public CupcakeShop() {
-        // Instantiates an instance of our baker as a local variable
-        Baker baker = new Baker();
+        myDecorator = new CS15Decorator();
+        
+    }
+    
+    @Override
+    public void order1() {
+        
+    }
 
-        //TODO Tell the baker to bake a cupcake
+    @Override
+    public void order2() {
+        
+    }
 
-        //TODO Have the decorator and froster do their jobs here
+    @Override
+    public void order3() {
+        
+    }
+
+    @Override
+    public void order4() {
+    
+    }
+
+    @Override
+    public void order5() {
+        
     }
 }
